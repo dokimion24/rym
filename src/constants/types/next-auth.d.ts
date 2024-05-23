78cfec6fd20c5b65f2fcc5954b20fb43ae0787ee
ss-davidlee <davidlee@styleseller.co.kr>
2024-05-23 18:25:10 +0900

    feat: Session type 수정

--- src/constants/types/next-auth.d.ts
+++ src/constants/types/next-auth.d.ts
@@ -3,10 +3,18 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      id: number;
-      name: string;
       email: string;
-      accessToken: string;
+      emailVerified: null | Date;
+      exp: number;
+      iat: number;
+      id: string;
+      image?: string;
+      jti: string;
+      name: string;
+      picture?: string;
+      sub: string;
+      createdAt: Date;
+      updatedAt: Date;
     };
   }
 }
