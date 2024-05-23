19b7c37d48861e80d5dea54fed7d16d968a67828
ss-davidlee <davidlee@styleseller.co.kr>
2024-05-23 18:22:03 +0900

    feat: profile 페이지 추가

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import LogoutButton from "@/components/auth/logoutButton";
+import React from "react";
+
+export default function ProfilePage() {
+  return (
+    <main className="pt-14 max-w-7xl mx-auto">
+      <LogoutButton />
+    </main>
+  );
+}
