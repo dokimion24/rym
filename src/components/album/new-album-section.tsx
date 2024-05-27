3afa7d25cda074f59c4c58bc81d197baf965ec08
ss-davidlee <davidlee@styleseller.co.kr>
2024-05-27 18:36:57 +0900

    chore: 주석처리

--- src/components/album/new-album-section.tsx
+++ src/components/album/new-album-section.tsx
@@ -1,34 +1,27 @@
-"use client";
+import albumApis from "@/apis/domain/album.api.server";
+import { Card } from "../ui";
+import Image from "next/image";
+import spotifyApi from "@/apis/config/spotifyApi.server";
+import albumApisServer from "@/apis/domain/album.api.server";
 
-import React, { useEffect, useState } from "react";
-import { reviewData } from "@/constants/review";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext } from "../ui";
-import albumApis from "@/apis/albumApis";
-import authApis from "@/apis/authApis";
+export default async function NewAlbumSection() {
+  // const data = await albumApisServer.getNewAlbum();
+  // console.log(data);
 
-const NewAlbumSection = () => {
-  const [albums, setAlbums] = useState(null);
-
-  useEffect(() => {
-    const fetchAlbums = async () => {
-      try {
-        const res = await authApis.getSpotifyAccessToken();
-        setAlbums(res.albums); // 상태를 업데이트합니다.
-      } catch (error) {
-        console.error("앨범 데이터를 불러오는데 실패했습니다:", error);
-      }
-    };
-
-    fetchAlbums();
-  }, []);
-
-  // 로딩 상태 혹은 에러 처리 등을 추가할 수 있습니다.
-  if (!albums) return <div>로딩 중...</div>;
   return (
     <section className="my-8">
-      <h2 className="text-xl font-bold py-4">최신 코멘트</h2>
+      <h2 className="text-xl font-bold py-4">최신 발매 앨범</h2>
+      {/* <Card>
+        <Image src={} />
+        <CardDescription className="mb-2">{description}</CardDescription>
+        <CardFooter className="flex items-center">
+          <Avatar className="w-8 h-8 inline-flex mr-2">
+            <AvatarImage src="https://github.com/shadcn.png" alt="shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <CardDescription>{username}</CardDescription>
+        </CardFooter>
+      </Card> */}
     </section>
   );
-};
-
-export default NewAlbumSection;
+}
