dd6afd157d41acd64a61d1977d49220a6264988a
ss-davidlee <davidlee@styleseller.co.kr>
2024-05-27 18:36:28 +0900

    feat: data fetching 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import NewAlbumSection from "@/components/album/new-album-section";
 import NewReviewSection from "@/components/review/new-review-section";
+import albumApisServer from "@/apis/domain/album.api.server";
+import authApisServer from "@/apis/domain/auth.api.server";
 
-export default function Home() {
+export default async function Home() {
+  const data = await authApisServer.getSpotifyAccessToken();
+  // console.log(data);
+
   return (
     <main className="pt-14 max-w-7xl mx-auto">
-      test
-      {/* <NewReviewSection /> */}
+      <NewReviewSection />
       <NewAlbumSection />
     </main>
   );
