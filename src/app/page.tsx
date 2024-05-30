89f58017fd66ad441d59a094872d6ce1809bc498
ss-davidlee <davidlee@styleseller.co.kr>
2024-05-30 10:05:38 +0900

    chore: 불필요한 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
+import { getSpotifyAccessToken } from "@/apis/domain/auth.api.server";
+import AuthWrapper from "@/components/@common/authWrapper";
+import SpotifyToken from "@/components/@common/spotifyToken";
 import NewAlbumSection from "@/components/album/new-album-section";
 import NewReviewSection from "@/components/review/new-review-section";
-import albumApisServer from "@/apis/domain/album.api.server";
-import authApisServer from "@/apis/domain/auth.api.server";
 
 export default async function Home() {
-  const data = await authApisServer.getSpotifyAccessToken();
-  // console.log(data);
-
   return (
     <main className="pt-14 max-w-7xl mx-auto">
+      <SpotifyToken />
       <NewReviewSection />
       <NewAlbumSection />
     </main>
