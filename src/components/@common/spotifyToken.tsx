282d8c844a115ddc405d4602f517496e26bea4aa
ss-davidlee <davidlee@styleseller.co.kr>
2024-05-30 10:08:21 +0900

    feat: SpotifyToken 컴포넌트 추가

---
+++ src/components/@common/spotifyToken.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import useSpotifyToken from "@/hooks/useSpotifyToken";
+import React from "react";
+
+const SpotifyToken = () => {
+  useSpotifyToken();
+
+  return <></>;
+};
+
+export default SpotifyToken;
