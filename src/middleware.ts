4dbff48b1076425463b807b6d7159931a77c8f70
ss-davidlee <davidlee@styleseller.co.kr>
2024-05-31 17:57:40 +0900

    feat: middleware 임시 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import { getSpotifyAccessToken } from "./apis/domain/auth.api.server";
 
-export function middleware(request: NextRequest) {
-  //   console.log(request);
-  //   let response = NextResponse.next();
-  //   response.cookies.set("test", "test");
-  //   return response;
+const getAccessToken = (req: NextRequest) =>
+  req.cookies.get("spotify-access-token")?.value;
+
+export function middleware(req: NextRequest) {
+  // req.cookies.get("myCookieName")?.value;
+  // request.cookies.getAll();
+  // let cookie = request.cookies.get("spotify-access-token");
+  // response.cookies.set("myCookieName", "some-value");
+  // console.log(response.cookies.getAll());
+
+  if (req.nextUrl.pathname == "/api/spotify-token") {
+    const accessToken = getAccessToken(req);
+  }
+  // if (!accessToken) {
+  //   const res = NextResponse.redirect(request.url);
+  //   res.cookies.set("code", "code value");
+  //   return res;
+  // }
+  return NextResponse.next();
 }
+
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
