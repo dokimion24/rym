1345a04673912b6f3e9fc7350d4500a87e8c5848
ss-davidlee <davidlee@styleseller.co.kr>
2024-06-12 16:16:19 +0900

    feat: api intereceptor 추가

--- src/apis/config/serviceApi.ts
+++ src/apis/config/serviceApi.ts
@@ -1,7 +1,31 @@
-import axios from "axios";
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+import { onResponse } from "./interceptor";
+import { getCookie } from "cookies-next";
+
+export const onRequestClient = async (config: InternalAxiosRequestConfig) => {
+  try {
+    const accessToken = getCookie("rym-access-token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const onResponseError = async (error: any) => {
+  if (error.request.status === 401) {
+    serviceApi.get("/auth/token");
+  }
+
+  return Promise.reject(error);
+};
 
 const serviceApi = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000/api",
 });
 
-export default serviceApi;
+serviceApi.interceptors.request.use(onRequestClient);
+serviceApi.interceptors.response.use(onResponse, onResponseError);
