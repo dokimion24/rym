4cf957572fb15ba5ecb47a1c773913da983b93a0
ss-davidlee <davidlee@styleseller.co.kr>
2024-06-12 16:15:54 +0900

    feat: next auth type 수정

--- src/constants/types/next-auth.d.ts
+++ src/constants/types/next-auth.d.ts
@@ -1,20 +1,25 @@
-import NextAuth from "next-auth";
+// next-auth.d.ts
+import { Session as NextAuthSession, User } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends NextAuthSession {
+    user: User;
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      email: string;
-      emailVerified: null | Date;
-      exp: number;
-      iat: number;
-      id: string;
-      image?: string;
-      jti: string;
-      name: string;
-      picture?: string;
-      sub: string;
-      createdAt: Date;
-      updatedAt: Date;
-    };
+    error?: "RefreshAccessTokenError";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    access_token: string;
+    expires_at: number;
+    refresh_token: string;
+    error?: "RefreshAccessTokenError";
   }
 }
